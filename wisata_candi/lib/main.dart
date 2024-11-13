572a9387bbc5277b8fcf74c2d785f82108479ced
KennyLie26 <kennylie_2226240094@mhs.mdp.ac.id>
2024-11-13 11:02:13 +0700

    part 4

--- wisata_candi/lib/main.dart
+++ wisata_candi/lib/main.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
-import 'package:wisata_candi/screens/detail_screen.dart';
 import 'package:wisata_candi/data/candi_data.dart';
+import 'package:wisata_candi/screens/detail_screen.dart';
+import 'package:wisata_candi/screens/profile_screen.dart';
+import 'package:wisata_candi/screens/signin_screen.dart';
 
 void main() {
   runApp(const MainApp());
@@ -12,14 +14,26 @@ class MainApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Wisata Candi',
+      title: 'wisata candi',
       theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
+        appBarTheme: const AppBarTheme(
+          iconTheme: IconThemeData(color: Colors.deepPurple),
+          titleTextStyle: TextStyle(
+            color: Color.fromARGB(255, 58, 116, 183),
+            fontSize: 20,
+            fontWeight: FontWeight.bold,
+          ),
+        ),
+        colorScheme: ColorScheme.fromSeed(seedColor:  const Color.fromARGB(255, 94, 169, 255)).copyWith(
+          primary: const Color.fromARGB(255, 58, 112, 183),
+          surface: Colors.deepPurple[50],
+        ),
+        useMaterial3: true
       ),
-      home: DetailScreen(
-        candi: candiList[0],
-      ),
+       home: DetailScreen(candi: candiList[0],),
+       //home: ProfileScreen(),
+      //home: SignInScreen(),
     );
   }
 }
+
